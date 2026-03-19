c1c80c715689b7ad4b651eb8604f9c09ca7e0a3f
Sudeep <lakrasudeep999@gmail.com>
2026-03-19 18:17:12 +0530

    Added inventory page and Product interaction

---
+++ pages/inventory_page.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+class InventoryPage:
+    def __init__(self,driver):
+        self.driver = driver
+
+    #locators
+    inventory_container = (By.ID,"inventory_container")
+    first_add_to_cart_button =(By.ID, "shopping_cart_container")
+
+    #methods
+    def is_inventory_page_loaded(self):
+        return self.driver.find_element(*self.inventory_container).is_displayed()
+
+    def add_first_product_to_cart(self) :
+        self.driver.find_element(*self.first_add_to_cart_button).click()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,21 @@
 from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
 
-
-def test_login(driver):
+def test_login_and_add_product(driver):
     driver.get("https://saucedemo.com")
 
     login = LoginPage(driver)
+    inventory = InventoryPage(driver)
 
     login.enter_username("standard_user")
     login.enter_password("secret_sauce")
     login.click_login()
 
-    assert "inventory" in driver.current_url
+    # assert "inventory" in driver.current_url
+
+    #validate inventory page
+    #print(driver.current_url)
+    assert inventory.is_inventory_page_loaded()
+
+    #add product
+    inventory.add_first_product_to_cart()
